879ef9d14173c9ba8102f155c7cf3efd2c2ae6b2
Patrick <patrikas.lyddon@gmail.com>
2023-10-11 14:44:36 +0100

    Added maud templating

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use axum::{routing::get, Router};
+use maud::{html, Markup};
+
+async fn hello_world() -> Markup {
+    html! {
+        h1 { "Hello, World!" }
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/hello", get(|| hello_world()));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
